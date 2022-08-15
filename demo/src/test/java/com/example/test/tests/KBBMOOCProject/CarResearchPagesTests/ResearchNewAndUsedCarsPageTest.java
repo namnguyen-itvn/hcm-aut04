fa2fe8b629f7f88e4694407ce88445810467400b
doanhoangtri <lfy.samsung5@gmail.com>
2022-08-16 00:25:12 +0700

    update

--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
@@ -23,5 +23,21 @@ public class ResearchNewAndUsedCarsPageTest extends KbbBaseTest {
        Assert.assertTrue(reviewPage.isBuildAndPriceBtnDisplayed());
        Assert.assertTrue(reviewPage.isSeeCarsForSaleBtnDisplayed());
     }
+
+
+    @Test(testName = "Verify Select a different year in list from Year dropdown located on top of Car Name on Review Page to change to a different vehicle Page with the chosen year in Review Page")
+    public void verifySelectADifferentYearInListFromYearDropdownLocatedOnTopOfCarNameOnReviewPageToChangeToADifferentVehiclePageWithTheChosenYearinReviewPage() throws InterruptedException{
+        
+       ResearchNewAndUsedCarsPage researchNewAndUsedCarsPage=new ResearchNewAndUsedCarsPage(driver);
+       researchNewAndUsedCarsPage.actionSelectaCars();
+       ReviewCarsPage reviewPage=new ReviewCarsPage(driver);
+       reviewPage.actionClickDropdowChangeYear();
+       Assert.assertTrue(reviewPage.isgetPageTitle2019HondaCivic());
+       Assert.assertTrue(reviewPage.assertIsTitleUsed2019HondaCivic());
+       Assert.assertTrue(reviewPage.isBuildAndPriceBtnDisplayedHonda2019());
+       Assert.assertTrue(reviewPage.isSeeCarsForSaleBtnDisplayedHonda2019());
+       
+       
+    }
 }
     
