2338ff1ba098a4ba899cdc898d14631bb0da62dc
Châu Minh Đô <110797648+KRJuuga@users.noreply.github.com>
2022-08-16 16:40:42 +0700

    Add Assertion in file CategoryAndStylePageTest.java in WhatMyCarWorthTest
    - Create folder WhatMyCarWorthPage in KBBMOOCProject folder in pages folder
    - Create file CategoryAndStylePage.java in WhatMyCarWorthPage folder
    - Create file MyCarValuePage.java in WhatMyCarWorthPage folder
    - Create file OptionPage.java in WhatMyCarWorthPage folder
    - Create folder WhatMyCarWorthTest in KBBMOOCProject folder in tests folder
    - Create file CategoryAndStylePageTest.java in WhatMyCarWorthTest
    - Delete file phpAccount.java in datadriven folder
    - Create file dataJson.java in datadriven folder
    - Delete SauceLabsFinalPages folder
    - Delete SauceLabsFinalTests folder
    - Delete target folder
    - Change url in configs.properties to https://staging.kbb.com/whats-my-car-worth/
    - Change keyword in file CustomKeyword.java from click to clickThenWait
    - Change chromedriver due to different version and system
    - Add Assertion in file CategoryAndStylePageTest.java in WhatMyCarWorthTest

--- demo/src/test/java/com/example/test/pages/KBBMOOCProject/WhatMyCarWorthPage/OptionPage.java
+++ demo/src/test/java/com/example/test/pages/KBBMOOCProject/WhatMyCarWorthPage/OptionPage.java
@@ -1,6 +1,7 @@
 package com.example.test.pages.KBBMOOCProject.WhatMyCarWorthPage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.example.test.pages.KBBMOOCProject.KbbBasePage;
 
@@ -11,4 +12,7 @@ public class OptionPage extends KbbBasePage {
         //TODO Auto-generated constructor stub
     }
     public String optionPageTitle = driver.getTitle();
+    WebElement xpathCategoryAndStyle = keyword.findWebElementByXpath("//div[@class='css-1itv5e3 eisth834']/p");
+    public String categoryAndStyleName = xpathCategoryAndStyle.getText();
+     
 }
