83ccd00d7f8c921ad39f697aa7eb1f64e94aaba0
DESKTOP-SO84L7T\DELL <bichphamCV@gmail.com>
2022-08-17 00:17:41 +0700

    Delete target folder Edit CustomKeyword Delete SauceLab Add DataDriven

---
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/WhatsMyCarWorthPageTest/WhatsMyCarWorthPageTest.java
@@ -0,0 +1,30 @@
+package com.example.test.tests.KBBMOOCProject.WhatsMyCarWorthPageTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.example.test.core.datadriven.TestDataProvider;
+import com.example.test.pages.KBBMOOCProject.WhatsMyCarWorthPage.WhatsMyCarWorthPage;
+import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
+
+public class WhatsMyCarWorthPageTest extends KbbBaseTest{
+  
+    @Test(testName = "Verify error message 'We couldn't find that VIN. Please re-enter.'should be shown when user enter invalid VIN code", dataProviderClass = TestDataProvider.class, dataProvider = "dataErrorMessageVIN")
+    public void verifyErrorMessageWeCouldntFindThatVINPleaseReEnterShouldBeShownWhenUserEnterInvalidVINCode(String invalidVIN, String email, String errorMessageVIN) throws InterruptedException {
+        WhatsMyCarWorthPage whatsMyCarWorthPage = new WhatsMyCarWorthPage(driver);
+        whatsMyCarWorthPage.sendInvalidKeysToVINTextbox(invalidVIN);
+        whatsMyCarWorthPage.sendKeysToEmailTextbox(email);
+        whatsMyCarWorthPage.clickOnVINGoButton();
+        Assert.assertTrue(whatsMyCarWorthPage.compareErrorMessage(errorMessageVIN));
+    }   
+    @Test(testName = "Verify error message 'Email is invalid.'should be shown when user enter invalid email", dataProviderClass = TestDataProvider.class, dataProvider = "dataErrorMessageEmail")
+    public void verifyErrorMessageEmailIsInvalidShouldBeShownWhenUserEnterInvalidEmail(String plate, String invalidEmail, String errorMessageEmail) throws InterruptedException {
+        WhatsMyCarWorthPage whatsMyCarWorthPage = new WhatsMyCarWorthPage(driver);
+        whatsMyCarWorthPage.clickOnLicensePlateCheckbox();
+        whatsMyCarWorthPage.sendKeysToTextboxes(plate, invalidEmail);
+        whatsMyCarWorthPage.selectState();
+        whatsMyCarWorthPage.selectHIoption();
+        whatsMyCarWorthPage.clickOnLicensePlateGoButton();
+        Assert.assertTrue(whatsMyCarWorthPage.compareEmailErrorMessage(errorMessageEmail));
+    }
+}
