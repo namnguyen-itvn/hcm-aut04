2e0c118eebef10f346f2f069eab462cc0dcc454b
DESKTOP-2V4TNT3\Admin <hungphamhltn6@gmail.com>
2022-08-18 17:19:48 +0700

    Update code, delete target file,

--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
@@ -3,7 +3,7 @@ package com.example.test.tests.KBBMOOCProject.CarResearchPagesTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.example.test.core.datadriven.ZipcodeForCarResearch;
+import com.example.test.core.datadriven.GetReviewCarResearch;
 import com.example.test.pages.KBBMOOCProject.CarResearchPages.ResearchNewAndUsedCarsPage;
 import com.example.test.pages.KBBMOOCProject.CarResearchPages.ReviewCarsPage;
 import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
@@ -13,13 +13,13 @@ import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
 public class ResearchNewAndUsedCarsPageTest extends KbbBaseTest {
     
     
-    @Test(testName = "Verify the Car Review Page should be displayed after click Get Reviews Button using valid data",dataProviderClass = ZipcodeForCarResearch.class, dataProvider = "zipcode")
+    @Test(testName = "Verify the Car Review Page should be displayed after click Get Reviews Button using valid data",dataProviderClass = GetReviewCarResearch.class, dataProvider = "getreview")
     public void VerifytheCarReviewPageshouldbedisplayedafterclickGetReviewsButtonusingvaliddata(String zipcode) throws InterruptedException{
         
        ResearchNewAndUsedCarsPage researchNewAndUsedCarsPage=new ResearchNewAndUsedCarsPage(driver);
        researchNewAndUsedCarsPage.actionSelectaVehicle();
        researchNewAndUsedCarsPage.actionInputZipcodeCarResearchPage(zipcode);
-       researchNewAndUsedCarsPage.actionClickGetReviewBtnCarResearchPage(zipcode);
+       researchNewAndUsedCarsPage.actionClickGetReviewBtnCarResearchPage();
        ReviewCarsPage reviewPage=new ReviewCarsPage(driver);
        Assert.assertTrue(reviewPage.isgetPageTitle());
        Assert.assertTrue(reviewPage.isgetComponentTitle());
