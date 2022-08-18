c73e5c8ab317be9f32456f1e7c5d2953b311905a
doanhoangtri <lfy.samsung5@gmail.com>
2022-08-18 22:31:47 +0700

    delete thred.sleep

--- demo/src/test/java/com/example/test/pages/KBBMOOCProject/CarResearchPages/ResearchNewAndUsedCarsPage.java
+++ demo/src/test/java/com/example/test/pages/KBBMOOCProject/CarResearchPages/ResearchNewAndUsedCarsPage.java
@@ -13,28 +13,15 @@ public class ResearchNewAndUsedCarsPage extends KbbBasePage {
         //TODO Auto-generated constructor stub
     }
 
-    WebElement selectYear = keyword.findWebElementByXpath("//select[@placeholder='Year']");
-    WebElement selectMake = keyword.findWebElementByXpath("//div[@class='make']//select[@placeholder='Make']");
-    WebElement selectModel = keyword.findWebElementByXpath("//div[@class='model']//select[@placeholder='Model']");
-    WebElement inputZip = keyword.findWebElementByXpath("//input[@type='tel']");
-    WebElement btnGetReview = keyword.findWebElementByXpath("//form//button[@type='submit']");
+  private  WebElement selectYear = keyword.findWebElementByXpath("//select[@placeholder='Year']");
+  private  WebElement selectMake = keyword.findWebElementByXpath("//div[@class='make']//select[@placeholder='Make']");
+ private   WebElement selectModel = keyword.findWebElementByXpath("//div[@class='model']//select[@placeholder='Model']");
+   private WebElement inputZip = keyword.findWebElementByXpath("//input[@type='tel']");
+  private  WebElement btnGetReview = keyword.findWebElementByXpath("//form//button[@type='submit']");
 
     
     public void actionSelectaCars(String zipcode) throws InterruptedException{
           
-        // keyword.scrollAndWaitToClick(selectYear);
-        // WebElement lbYear = keyword.findWebElementByXpath("//select/option[@value='2018']");
-        // keyword.scrollAndWaitToClick(lbYear);
-        // keyword.scrollAndWaitToClick(selectMake);
-        // WebElement lbMake = keyword.findWebElementByXpath("//select/option[text()='Honda']");
-        // keyword.scrollAndWaitToClick(lbMake);
-        // keyword.scrollAndWaitToClick(selectModel);
-        // WebElement lbModel = keyword.findWebElementByXpath("//select/option[text()='Civic']");
-        // keyword.scrollAndWaitToClick(lbModel);
-        // keyword.sendKeys(inputZip, "43446");
-        // keyword.scrollAndWaitToClick(btnGetReview);
-
-
         keyword.selectElement(selectYear, "2018");
         keyword.selectElement(selectMake, "Honda");
         keyword.selectElement(selectModel, "Civic");
