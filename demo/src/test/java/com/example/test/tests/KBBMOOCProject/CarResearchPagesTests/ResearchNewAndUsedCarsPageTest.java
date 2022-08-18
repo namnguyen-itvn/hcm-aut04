7ec784daaa11827c51f965f131655bbeefe56932
doanhoangtri <lfy.samsung5@gmail.com>
2022-08-18 15:20:11 +0700

    update testcase MProject

--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
@@ -3,7 +3,7 @@ package com.example.test.tests.KBBMOOCProject.CarResearchPagesTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.example.test.core.datadriven.phpAccount;
+import com.example.test.core.datadriven.CarResearchZipCode;
 import com.example.test.pages.KBBMOOCProject.CarResearchPages.ResearchNewAndUsedCarsPage;
 import com.example.test.pages.KBBMOOCProject.CarResearchPages.ReviewCarsComponent;
 import com.example.test.pages.KBBMOOCProject.CarResearchPages.ReviewCarsPage;
@@ -14,7 +14,7 @@ import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
 public class ResearchNewAndUsedCarsPageTest extends KbbBaseTest {
     
 
-    @Test(testName = "Verify Select a different year in list from Year dropdown located on top of Car Name on Review Page to change to a different vehicle Page with the chosen year in Review Page",dataProviderClass =   phpAccount.class, dataProvider = "inputzipcode")
+    @Test(testName = "Verify Select a different year in list from Year dropdown located on top of Car Name on Review Page to change to a different vehicle Page with the chosen year in Review Page",dataProviderClass =   CarResearchZipCode.class, dataProvider = "inputzipcode")
     public void verifySelectADifferentYearInListFromYearDropdownLocatedOnTopOfCarNameOnReviewPageToChangeToADifferentVehiclePageWithTheChosenYearinReviewPage(String zipcode) throws InterruptedException{
         
        ResearchNewAndUsedCarsPage researchNewAndUsedCarsPage=new ResearchNewAndUsedCarsPage(driver);
