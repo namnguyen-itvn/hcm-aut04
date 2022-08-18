51866e6ec1286be89cefa26417f5546fba0ad37a
LEHOA705 <86782791+LEHOA705@users.noreply.github.com>
2022-08-18 17:40:40 +0700

    Update Testcase

---
+++ demo/src/test/java/com/example/test/pages/KBBMOOCProject/ButtonStyles.java
@@ -0,0 +1,20 @@
+package com.example.test.pages.KBBMOOCProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ButtonStyles extends KbbBasePage {
+
+    public ButtonStyles(WebDriver driver) {
+        super(driver);
+        //TODO Auto-generated constructor stub
+    }
+     WebElement btnStyles = keyword.findWebElementByXpath("//div[@label='Styles']");
+   
+public void clickOnButtonStyles () throws InterruptedException{
+    
+    keyword.scrollAndWaitToClick(btnStyles);
+    
+}
+
+}
--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/TestCarResearch/CarRearchTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/TestCarResearch/CarRearchTest.java
@@ -3,22 +3,22 @@ package com.example.test.tests.KBBMOOCProject.TestCarResearch;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.example.test.core.datadriven.phpAccount;
+import com.example.test.core.datadriven.CarResearch;
 import com.example.test.pages.CompareTitleCar;
+import com.example.test.pages.KBBMOOCProject.ButtonCarChevroletCorvette;
+import com.example.test.pages.KBBMOOCProject.ButtonStyles;
 import com.example.test.pages.KBBMOOCProject.SearchCarPage;
 import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
 
 public class CarRearchTest extends KbbBaseTest {
-    @Test(testName = "Verify Cars Review page  title is display when user click on item car name as Chevrolet Corvette of component Other Compact Coupes", dataProviderClass = phpAccount.class, dataProvider = "zip")
+    @Test(testName = "Verify Cars Review page  title is display when user click on item car name as Chevrolet Corvette of component Other Compact Coupes", dataProviderClass = CarResearch.class, dataProvider = "CarResearchPages")
     public void VerifyCarsReviewPageTitleIsDisplayWhenUserClickOnItemCarNameAsChevroletCorvetteOfComponentOtherCompactCoupesverifyProductsPageTitleWhenLoginSuccess(String zipcode) throws InterruptedException{
         SearchCarPage CarSearch = new SearchCarPage(driver);
-        CarSearch.SelectYear();
-        CarSearch.SelectMake();
-        CarSearch.SelectModel();
-        CarSearch.ZipCode(zipcode);
-        CarSearch.clickOnGetReview();
-        CarSearch.clickonStyles();
-        CarSearch.clickonCarChevrolet();
+        CarSearch.actionSelectaCars(zipcode);
+        ButtonStyles buttonStyles =new ButtonStyles(driver);
+        buttonStyles.clickOnButtonStyles();
+        ButtonCarChevroletCorvette buttonCarchevloca=new ButtonCarChevroletCorvette(driver);
+        buttonCarchevloca.clickOnButtonCar();
         CompareTitleCar Car = new CompareTitleCar(driver);
         Assert.assertTrue(Car.isgetPageTitle());
         Assert.assertTrue(Car.isgetPageTitleUrl());
