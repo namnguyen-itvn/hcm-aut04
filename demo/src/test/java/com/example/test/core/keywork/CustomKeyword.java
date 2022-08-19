ef0573373feaaf91a33f0d4c3609767c4c45a324
TGiang2002 <doantrucgiang2002@gmail.com>
2022-08-19 12:27:53 +0700

    update conflict

--- demo/src/test/java/com/example/test/core/keywork/CustomKeyword.java
+++ demo/src/test/java/com/example/test/core/keywork/CustomKeyword.java
@@ -1,6 +1,5 @@
 package com.example.test.core.keywork;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -8,7 +7,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,7 +15,7 @@ public class CustomKeyword {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public CustomKeyword(WebDriver driver,WebDriverWait wait) {
+    public CustomKeyword(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
@@ -32,36 +30,7 @@ public class CustomKeyword {
         {
             driver.get(baseUrl);
         }
-        throw new Exception("url not start with http or http. Double check baseUrl");
-    }
-
-    /**
-     * Wait for Element displayed and input text
-     * @param element
-     * @param text
-     * @throws InterruptedException
-     */
-    public void sendKeys(WebElement element, String text) throws InterruptedException{
-        try{
-            waitForElementIsDisplayed(element).sendKeys(text);
-            Thread.sleep(3000);
-        }catch(WebDriverException ex){
-            throw new WebDriverException("Element not availabe to input text!");
-        }
-    }
-
-    /**
-     * find element by css selector
-     * 
-     * @param locator element locator
-     * @return element to be located
-     */
-    public WebElement findElementByCssSelector(String locator) {
-        try{
-            return wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(locator)));
-        } catch (WebDriverException e) {
-            throw new WebDriverException("Element is not displayed ");
-        }
+        throw new Exception("url not start with http or https. Double check baseUrl");
     }
 
     /**
@@ -71,113 +40,44 @@ public class CustomKeyword {
      */
     public WebElement findWebElementByXpath(String locator){
         try{
+            scrollToElemtnIntoView(driver.findElement(By.xpath(locator)));
             return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+        } catch(WebDriverException ex ){
+            throw new WebDriverException("Element not found!");
+        }
+    }
+    /**
+     * Scroll to element and find element by classname
+     * @param locator
+     * @return
+     */
+    public WebElement findWebElementByClassName(String locator){
+        try{
+            scrollToElemtnIntoView(driver.findElement(By.className(locator)));
+            return wait.until(ExpectedConditions.presenceOfElementLocated(By.className(locator)));
         } catch(WebDriverException ex ){
             throw new WebDriverException("Element not found!");
         }
     }
 
     /**
-     * Scroll to element and find element by xpath
+     * Scroll to element and find element by css
      * @param locator
      * @return
      */
-    public List<WebElement> findListWebElementByXpath(String locator){
+    public WebElement findWebElementByCSS(String locator){
         try{
-            return driver.findElements(By.xpath(locator));
+            scrollToElemtnIntoView(driver.findElement(By.cssSelector(locator)));
+            return wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(locator)));
         } catch(WebDriverException ex ){
-            throw new WebDriverException("Elements not found!");
+            throw new WebDriverException("Element not found!");
         }
     }
 
     /**
-     * Scroll/Move to element
-     * 
-     * @param element to find in page
-     */
-    public void scrollToElement(WebElement element){
-
-        try {
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("arguments[0].scrollIntoView(true);", element);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        } catch (WebDriverException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Check element is display :v
-     * 
+     * Scroll to element into View
      * @param element
-     * @return
      */
-    public boolean isDisplay(WebElement element){
-        return waitForElementIsDisplayed(element).isDisplayed();
-    }
-
-    /**
-     * Wait to element visible
-     * 
-     * @param element to check display and clickable 
-     * @return element to be located
-     */
-    public WebElement waitForElementIsDisplayed(WebElement element){
-        try {
-            return wait.until(ExpectedConditions.elementToBeClickable(element));
-        } catch (WebDriverException e) {
-            throw new WebDriverException("Element is not displayed ");
-        }
-    }
-
-    /**
-     * Check place holder equal given text or not
-     * 
-     * @param element
-     * @return
-     */
-    public boolean checkPlaceHolder(WebElement element, String text){
-        String attribute = waitForElementIsDisplayed(element).getAttribute("placeholder");
-        return (attribute.equals(text));
-    }
-
-    /**
-     * Click to element
-     * 
-     * @param element to click
-     * @throws InterruptedException
-     */
-    public CustomKeyword click(WebElement element) throws InterruptedException{
-        waitForElementIsDisplayed(element).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return new CustomKeyword(driver,wait);
-    }
-
-    public void selectElement(WebElement element, String text) throws InterruptedException{
-        try{
-            waitForElementDisplayed(element);
-            Select select6=new Select(element);
-            select6.selectByVisibleText(text);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            }catch(WebDriverException ex){
-            throw new WebDriverException("Element not availabe to input text!");
-        }
-    }
-
-    
-    public WebElement waitForElementDisplayedWithoutScroll(WebElement element) {
-
-        try{
-
-            return wait.until(ExpectedConditions.elementToBeClickable(element));
-
-        } catch(WebDriverException ex){
-
-            throw new WebDriverException("Element not displayed");
-
-        }  
-
-    }
     public void scrollToElemtnIntoView(WebElement element){
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
     }
@@ -191,41 +91,110 @@ public class CustomKeyword {
         }
         
     }
+
+    //*wait without scroll */
+    public WebElement waitForElementDisplayedWithoutScroll(WebElement element) throws InterruptedException {
+        try{
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            return wait.until(ExpectedConditions.elementToBeClickable(element));
+        } catch(WebDriverException ex){
+            throw new WebDriverException("Element not displayed");
+        }  
+    }
+
+    /**
+     * Wait for Element displayed and input text
+     * @param element
+     * @param text
+     * @throws InterruptedException
+     */
+    public void sendKeys(WebElement element, String text) throws InterruptedException{
+        try{
+            waitForElementDisplayed(element).sendKeys(text);
+            Thread.sleep(3000);
+       }catch(WebDriverException ex){
+            throw new WebDriverException("Element not availabe to input text!");
+        }
+    }
+
     /**
      * Scroll And Wait To Click
      * @param element
      * @return
-     * @throws InterruptedException
      */
-    public CustomKeyword scrollAndWaitToClick(WebElement element) throws InterruptedException {
+    public CustomKeyword scrollAndWaitToClick(WebElement element) {
         try{
-            
             scrollToElemtnIntoView(element);
             waitForElementDisplayed(element);
             element.click();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             return new CustomKeyword(driver, wait);
         }catch(WebDriverException ex){
-            throw new WebDriverException("Element not availabe to input text!");
+            throw new WebDriverException("Element not availabe to click!");
+        }
+    }
+
+    /**
+     * Wait To Click
+     * @param element
+     * @return
+     * @throws InterruptedException
+     */
+    public CustomKeyword waitToClick(WebElement element) throws InterruptedException {
+        try{
+            waitForElementDisplayedWithoutScroll(element);
+            element.click();
+            return new CustomKeyword(driver, wait);
+        }catch(WebDriverException ex){
+            throw new WebDriverException("Element not availabe to click!");
         }
     }
     public void scrollByToClick(WebElement element) throws InterruptedException{
         try{
             JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("window.scrollBy(0,500)");
+            js.executeScript("window.scrollBy(0,200)");
+           
             element.click();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             }catch(WebDriverException ex){
             throw new WebDriverException("Element not availabe to input text!");
         }
     }
-   
     public void argumentsByToClick(WebElement element) throws InterruptedException{
         try{
             JavascriptExecutor jse = (JavascriptExecutor)driver;
             jse.executeScript("arguments[0].click()", element);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             }catch(WebDriverException ex){
+            throw new WebDriverException("Element not availabe to input text!");
+        }
+    }
+    public void scrollBackToClick(WebElement element) throws InterruptedException{
+        try{
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0,0)");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            element.click();
+            }catch(WebDriverException ex){
+            throw new WebDriverException("Element not availabe to input text!");
+        }
+    }
+    public void selectElement(WebElement element, String text) throws InterruptedException{
+        try {
+            //waitForElementDisplayed(element);
+            Select select6=new Select(element);
+            select6.selectByVisibleText(text);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }catch(WebDriverException ex){
+            throw new WebDriverException("Element not availabe to input text!");
+        }
+ 
+    }
+    public void click(WebElement element){
+        try{
+            waitForElementDisplayed(element);
+            element.click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }catch(WebDriverException ex){
             throw new WebDriverException("Element not availabe to input text!");
         }
     }
---
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/CarResearchPagesTests/ResearchNewAndUsedCarsPageTest.java
@@ -0,0 +1,31 @@
+package com.example.test.tests.KBBMOOCProject.CarResearchPagesTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.example.test.core.datadriven.GetReviewCarResearch;
+import com.example.test.pages.KBBMOOCProject.CarResearchPages.ResearchNewAndUsedCarsPage;
+import com.example.test.pages.KBBMOOCProject.CarResearchPages.ReviewCarsPage;
+import com.example.test.tests.KBBMOOCProject.KbbBaseTest;
+
+
+
+public class ResearchNewAndUsedCarsPageTest extends KbbBaseTest {
+    
+    
+    @Test(testName = "Verify the Car Review Page should be displayed after click Get Reviews Button using valid data",dataProviderClass = GetReviewCarResearch.class, dataProvider = "getreview")
+    public void VerifytheCarReviewPageshouldbedisplayedafterclickGetReviewsButtonusingvaliddata(String zipcode) throws InterruptedException{
+        
+       ResearchNewAndUsedCarsPage researchNewAndUsedCarsPage=new ResearchNewAndUsedCarsPage(driver);
+       researchNewAndUsedCarsPage.actionSelectaVehicle();
+       researchNewAndUsedCarsPage.actionInputZipcodeCarResearchPage(zipcode);
+       researchNewAndUsedCarsPage.actionClickGetReviewBtnCarResearchPage();
+       ReviewCarsPage reviewPage=new ReviewCarsPage(driver);
+       Assert.assertTrue(reviewPage.isgetPageTitle());
+       Assert.assertTrue(reviewPage.isgetComponentTitle());
+       Assert.assertTrue(reviewPage.isBuildAndPriceBtnDisplayed());
+       Assert.assertTrue(reviewPage.isSeeCarsForSaleBtnDisplayed());
+    }
+
+}
+    
