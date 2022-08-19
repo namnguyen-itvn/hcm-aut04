7b28d037acf74d095dce531a9d4830ab7efe4eda
DuyH2 <huynhduy25019@gmail.com>
2022-08-19 11:49:00 +0700

    * update kbbBaseTest

--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/KbbBaseTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/KbbBaseTest.java
@@ -13,7 +13,7 @@ public class KbbBaseTest {
     public WebDriver driver;
     public CustomKeyword keyword;
     public String pathChromeDriver = "src\\test\\java\\com\\example\\test\\core\\drivers\\driver\\chromedriver.exe";
-    public String baseUrl = ReadConfigPropertyFile.getPropertyValue("url");
+    public String baseUrl = ReadConfigPropertyFile.getPropertyValue("CarForSaleUrl");
     public String browser = ReadConfigPropertyFile.getPropertyValue("browser");
     
     @BeforeTest
