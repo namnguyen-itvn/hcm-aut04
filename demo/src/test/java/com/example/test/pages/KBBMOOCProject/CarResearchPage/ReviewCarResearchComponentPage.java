2fe64a1de57ec793cfc28e2401e796d600fe3be8
DESKTOP-G28BQF6\ĐÀO ANH SÁNG <daoanhsang081099@gmail.com>
2022-08-19 13:18:55 +0700

    update testcase

--- demo/src/test/java/com/example/test/pages/KBBMOOCProject/CarResearchPage/ReviewCarResearchComponentPage.java
+++ demo/src/test/java/com/example/test/pages/KBBMOOCProject/CarResearchPage/ReviewCarResearchComponentPage.java
@@ -1,4 +1,4 @@
-package com.example.test.pages.KBBMOOCProject.CarResearchPages;
+package com.example.test.pages.KBBMOOCProject.CarResearchPage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
