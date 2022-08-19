27d4817e09d496b11a9a0539ad08dc459403f230
Châu Minh Đô <110797648+KRJuuga@users.noreply.github.com>
2022-08-19 12:41:26 +0700

    Resolve Conflict

--- demo/src/test/java/com/example/test/tests/KBBMOOCProject/KbbBaseTest.java
+++ demo/src/test/java/com/example/test/tests/KBBMOOCProject/KbbBaseTest.java
@@ -12,7 +12,7 @@ public class KbbBaseTest {
     public WebDriver driver;
     public CustomKeyword keyword;
     public String pathChromeDriver = "src\\test\\java\\com\\example\\test\\resources\\driver\\chromedriver.exe";
-    public String baseUrl = ReadConfigPropertyFile.getPropertyValue("urlforwhatmycarworth");
+    public String baseUrl = ReadConfigPropertyFile.getPropertyValue("url");
     public String browser = ReadConfigPropertyFile.getPropertyValue("browser");
     
     @BeforeTest
