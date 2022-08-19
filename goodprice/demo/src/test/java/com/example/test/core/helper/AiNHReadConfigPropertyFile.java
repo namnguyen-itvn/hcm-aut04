b49ad9c14df090403f217f8c76fe8e224cf8df08
DESKTOP-QL57G36\hoaia <hoaiaiht98@gmail.com>
2022-08-19 14:33:43 +0700

    Change name class

---
+++ goodprice/demo/src/test/java/com/example/test/core/helper/AiNHReadConfigPropertyFile.java
@@ -0,0 +1,25 @@
+package com.example.test.core.helper;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ReadConfigPropertyFile {
+
+    public static String propertiesFilePath ="src\\test\\java\\com\\example\\test\\core\\ainhconfigs.properties";
+    static String projectPath = System.getProperty("user.dir") + "\\";
+
+    public static String getPropertyValue(String atrName){
+        String value = null;
+        try(FileInputStream input = new FileInputStream(propertiesFilePath)){
+            Properties properties = new Properties();
+            properties.load(input);
+            value = properties.getProperty(atrName);
+
+        }catch(Exception ex)
+        {
+            System.out.println(ex);
+            System.out.println(ex.getMessage());
+        }
+        return value;
+    }
+}
