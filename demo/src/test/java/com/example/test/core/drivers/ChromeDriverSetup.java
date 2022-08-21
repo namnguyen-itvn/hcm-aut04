272affb9098cf311b45b2bb6d1158af554b48aea
TGiang2002 <doantrucgiang2002@gmail.com>
2022-08-21 20:24:31 +0700

    update code after conflict

--- demo/src/test/java/com/example/test/core/drivers/ChromeDriverSetup.java
+++ demo/src/test/java/com/example/test/core/drivers/ChromeDriverSetup.java
@@ -19,7 +19,7 @@ public class ChromeDriverSetup implements SeleniumDriver {
             options = new ChromeOptions();
             options.addArguments("start-maximized");
             options.setAcceptInsecureCerts(true);
-            File file = new File("src\\test\\java\\com\\example\\test\\core\\drivers\\driver\\extension_3_14_1_0.crx");
+            File file = new File("src\\test\\java\\com\\example\\test\\core\\drivers\\adblock_1_42_4_0_chrome.crx");
             options.addExtensions(file);
 
         }
